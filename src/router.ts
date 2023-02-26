20a8d5d689cd28bbae623992dce58d9b6b34e20d
KaiKai <kaikai_miniworld@outlook.com>
2023-02-26 16:32:39 +0800

    修复“无机器人时操作报错”的问题 (#8)
    现在无机器人时进入操作页面，会跳转到添加机器人页面。

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
+import { useAccountStore } from './stores/account';
+import { Message } from '@arco-design/web-vue';
 
 export const routes: RouteRecordRaw[] = [
   {
@@ -28,6 +30,7 @@ export const routes: RouteRecordRaw[] = [
         meta: { title: '发送消息' },
       },
     ],
+    meta: { needLogin: true },
   },
   {
     path: '/:catchAll(.*)',
@@ -35,7 +38,25 @@ export const routes: RouteRecordRaw[] = [
   },
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.needLogin === true) { // 需要登录
+    if (!useAccountStore().activeBotToken) { // 未登录
+      Message.warning({
+        content: '请先添加机器人',
+        duration: 3000,
+      });
+      next('/login');
+      return; // next 只能调用一次
+    }
+  }
+  next();
+});
+
+export { router };
+
+export default router;
