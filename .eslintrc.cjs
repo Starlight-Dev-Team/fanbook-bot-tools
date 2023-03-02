7ac249aa8c3201890eef9551a61de6e089cce365
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-02 21:58:15 +0800

    修改 ESLint 配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   },
   rules: {
     'promise/param-names': [0],
+    'no-return-assign': [0],
     '@typescript-eslint/comma-dangle': [2, 'always-multiline'],
     '@typescript-eslint/object-curly-spacing': [2, 'always'],
     '@typescript-eslint/member-delimiter-style': [2, {
@@ -24,6 +25,7 @@ module.exports = {
       'singleline': { 'delimiter': 'semi', 'requireLast': true },
     }],
     '@typescript-eslint/naming-convention': [0],
+    '@typescript-eslint/no-confusing-void-expression': [0],
     '@typescript-eslint/no-debugger': production ? [2] : [0],
     '@typescript-eslint/no-floating-promises': [0],
     '@typescript-eslint/promise-function-async': [0],
