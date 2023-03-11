473208c58f6f2dd5559fd348c793d19479db5f71
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-11 13:20:05 +0800

    修复机器人令牌无效时一直加载的问题

--- stores/account.ts
+++ stores/account.ts
@@ -3,19 +3,33 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 import { getBots } from '~~/utils/account';
+import { Bot } from '@starlight-dev-team/fanbook-api-sdk';
+import type { Profile } from '@starlight-dev-team/fanbook-api-sdk/dist/types';
 
 export const useAccountStore = defineStore('account', () => {
+  const botProfiles: Ref<Record<string, Profile>> = ref({});
   return {
     botTokens: ref([] as string[]),
+    botProfiles,
     activeBotToken: ref(undefined as string | undefined),
   };
 });
 
-export function initAccountStore() {
-  const botTokens = getBots();
+export async function initAccountStore() {
+  const tokens = getBots();
+  const botTokens: string[] = [];
+  const botProfiles: Record<string, Profile> = {};
+  for (const token of tokens) { // 过滤无效令牌、获取机器人信息
+    try {
+      botProfiles[token] = await (new Bot(token)).getProfile();
+      botTokens.push(token);
+    } catch {}
+  }
+  setBots(botTokens); // 覆盖回去，就去除了无效的令牌
   useAccountStore().$patch({
     botTokens,
-    activeBotToken: botTokens[0] ?? undefined,
+    botProfiles,
+    activeBotToken: tokens[0] ?? undefined,
   });
 }
 
