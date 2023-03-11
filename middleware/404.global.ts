4530bc439c253062a5d4b981e7f515439f93d72d
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-11 11:13:51 +0800

    修复 ESLint 错误

--- middleware/404.global.ts
+++ middleware/404.global.ts
@@ -2,5 +2,5 @@ export default defineNuxtRouteMiddleware(() => {
   useRouter().addRoute({
     path: '/:catchAll(.*)',
     redirect: '/404',
-  })
+  });
 });
--- plugins/device.client.ts
+++ plugins/device.client.ts
@@ -9,5 +9,5 @@ export default defineNuxtPlugin(() => {
   }
   return {
     provide: { device },
-  }
+  };
 });
