8081e28f7863c869719191a97af824701cf70594
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-11 13:00:58 +0800

    修复格式错误

--- plugins/device.client.ts
+++ plugins/device.client.ts
@@ -1,11 +1,11 @@
 export type DeviceType = 'desktop' | 'mobile';
 
 export default defineNuxtPlugin(() => {
-  const device: DeviceType = (() => {
-    return (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
-    ).test(navigator.userAgent) ? 'mobile' : 'desktop';
-  })();
+  const device: DeviceType = (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
+  ).test(navigator.userAgent)
+    ? 'mobile'
+    : 'desktop';
   document.body.classList.add(device);
   return {
     provide: { device },
