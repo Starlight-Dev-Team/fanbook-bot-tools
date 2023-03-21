c1c3178ac7da9663ef20ba6c78274594e07b6f41
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-21 22:29:45 +0800

    修复 ESLint 错误

--- plugins/debug.client.ts
+++ plugins/debug.client.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
   const debug = getDebugMode();
   initDebugger();
-  if (debug === true) {
+  if (debug) {
     console.warn('请勿在调试面板执行不可信的命令！');
     console.info('提示：在 command 处输入 %cqd()%c ，按 OK 退出调试模式', 'font-size: 24px; font-style: italic; color: blue;', 'all: unset;');
     window.qd = function() {
