5a7f302a3f757cd27e2171e74fb01fb95e4fb358
KaiKai <kaikai_miniworld@outlook.com>
2023-03-21 22:31:47 +0800

    添加调试模式 (#52)

---
+++ pages/debug.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { getDebugMode, setDebugMode } from '~~/utils/debug';
+
+import {
+  Form,
+  FormItem,
+  Switch,
+} from '@arco-design/web-vue';
+
+definePageMeta({
+  title: '调试模式',
+})
+
+const form = reactive({
+  debug: getDebugMode(),
+});
+
+function changeDebugMode(value: boolean) {
+  setDebugMode(value);
+  location.reload();
+}
+</script>
+
+<template>
+  <Form class='form' :model='form'>
+    <FormItem label='调试模式'>
+      <Switch
+        v-model='form.debug'
+        @change='(v) => changeDebugMode(!!v)'
+      />
+    </FormItem>
+  </Form>
+</template>
+
+<style scoped>
+
+.form {
+  display: block;
+  width: 70%;
+  margin: 0 auto;
+}
+body.mobile .form {
+  width: 90vw;
+}
+</style>
---
+++ plugins/debug.d.ts
@@ -0,0 +1,3 @@
+declare interface Window {
+  qd: () => void;
+}
