aa4d0de7ec7743513aa6afac7f0e329321530d18
KaiKai <kaikai_miniworld@outlook.com>
2023-04-05 10:18:23 +0800

    修复“无机器人时报错 500”的问题 (#71)

--- utils/bot.ts
+++ utils/bot.ts
@@ -2,17 +2,14 @@ import { Bot } from '@starlight-dev-team/fanbook-api-sdk';
 
 import useAccountStore from '~~/stores/account';
 
-class UnauthorizedError extends Error {
-  constructor() {
-    super('No active bot given');
-  }
-}
-
 let currentBot: Bot | undefined;
 export function getCurrentBot(): Bot {
   const token = useAccountStore().activeBotToken;
   if (!currentBot) {
-    if (!token) throw new UnauthorizedError();
+    if (!token) { // 无机器人
+      console.warn('No active bot given');
+      return new Bot('0'); // 兜底方案
+    }
     currentBot = new Bot(token);
   }
   return currentBot;
