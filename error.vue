1ecbe67f0c408c524c3b8bb3ccf6bd9926360fe9
KaiKai <kaikai_miniworld@outlook.com>
2023-05-24 20:54:27 +0800

    修复 404 需要点两次“返回上一页”的问题 (#80)

--- error.vue
+++ error.vue
@@ -6,13 +6,15 @@ export interface Props {
 }
 const props = defineProps<Props>();
 
+type Type = undefined | '404';
+const type = ref(undefined as Type);
 switch (typeof props.error) {
   case 'undefined':
   case 'string':
   case 'object': { // 是 Nuxt 错误
     if (props.error !== null && isNuxtError(props.error)) {
       // 404
-      if (props.error.statusCode === 404) navigateTo('/404');
+      if (props.error.statusCode === 404) type.value = '404';
     }
   }
 }
@@ -27,10 +29,11 @@ console.error(props.error);
 </script>
 
 <template>
-  <Result status='error' title='未知错误'>
+  <Result status='error' title='未知错误' v-if='!type'>
     <template #extra>
       <Button type='primary' @click='() => navigateTo("/")'>返回首页</Button>
       <Button @click='handleReportError'>报告错误</Button>
     </template>
   </Result>
+  <Page404 v-else-if='type === "404"' />
 </template>
--- middleware/404.global.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  useRouter().addRoute({
-    path: '/:catchAll(.*)',
-    redirect: '/404',
-  });
-});
