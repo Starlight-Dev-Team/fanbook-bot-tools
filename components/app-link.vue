bd95184d0c5b20664f59d4a0fed88ddcb6dc4769
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-05-30 22:13:58 +0800

    修复底部导航无法跳转外部链接的问题

--- components/app-link.vue
+++ components/app-link.vue
@@ -14,26 +14,24 @@ const props = withDefaults(defineProps<Props>(), {
   icon: false,
 });
 
-const external = ((): boolean => {
+const external = computed((): boolean => {
   try {
     new URL(props.to); // 此时给定一个参数，如果不是绝对地址，会报错
   } catch {
     return false; // 报错了，不是绝对地址
   }
   return true; // 没报错，是绝对地址
-})();
+});
 </script>
 
 <template>
-  <Link v-if='external' v-bind='$attrs' target='_blank' :href='to' :icon='icon'>
-    <slot />
-  </Link>
-  <NuxtLink v-else v-slot='{ navigate }' :to='to' custom>
+  <NuxtLink v-slot='{ navigate }' :to='to' custom :external='external'>
     <Link
       v-bind='$attrs'
       :href='to'
       :hoverable='hoverable'
       :icon='icon'
+      :target='external ? "_blank" : undefined'
       @click='navigate'
     >
       <slot />
