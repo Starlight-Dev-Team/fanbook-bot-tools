4b139a652dde2642a5179a496c202235cffc6f06
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-05-30 22:19:11 +0800

    删除开发环境 SSL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-
 export default defineNuxtConfig({
   ssr: false,
   modules: [
@@ -13,10 +10,4 @@ export default defineNuxtConfig({
       },
     ],
   ],
-  devServer: {
-    https: {
-      key: readFileSync(resolve(__dirname, './https/cert.key')).toString(),
-      cert: readFileSync(resolve(__dirname, './https/cert.crt')).toString(),
-    },
-  },
 });
